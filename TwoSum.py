dfbdf20c5c903b7a275a5ec4ffbdf9c0aacb3d2c
Naing Htet Zaw <nainghtetzaw20101@gmail.com>
2025-08-02 00:35:11 +0700

    finished: solving TwoSum solution (Two Pointers)

--- TwoSum.py
+++ TwoSum.py
@@ -2,4 +2,21 @@ from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        res = []
+        l, r = 0, len(numbers) - 1
+        
+        while l < r:
+            twoSum = numbers[l] + numbers[r]
+            
+            if twoSum > target:
+                r -= 1
+            elif twoSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
+                
+        return []
+    
+result = Solution.twoSum(Solution, [-5,-3,0,2,4,6,8], 5)
+
+for i in result:
+    print(i)
