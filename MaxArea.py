adfea6b2fcfab59b7744a5e90f1750e44660c80c
Naing Htet Zaw <nainghtetzaw20101@gmail.com>
2025-08-06 00:21:43 +0700

    successfully and effienciently solved the ContainerWithMoreWater problem

--- MaxArea.py
+++ MaxArea.py
@@ -1,20 +1,18 @@
 from typing import List
 
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        maxArea = 0
+    def maxArea(self, height: List[int]) -> int:        
+        maxArea, l, r = 0, 0, len(height) - 1
         
-        for i, a in enumerate(height):
-            r, j = len(height) - 1, height[len(height) - 1]
+        while l < r:
+            if maxArea < min(height[l], height[r]) * (r - l):
+                maxArea = min(height[l], height[r]) * (r - l)
             
-            while i < r:
-                if maxArea < min(a, height[r]) * (r - i):
-                    maxArea = min(a, height[r]) * (r - i)
-                    
+            if height[l] == height[r] or height[l] > height[r]:
                 r -= 1
-                while i < r and height[r] <= j:
-                    r -= 1 
+            elif height[r] > height[l]:
+                l += 1
         
         return maxArea
     
-print(Solution.maxArea(Solution, [1,8,6,2,5,4,8,3,7]))
+print(Solution.maxArea(Solution, [1,8,6,2,5,4,8,3,1,7]))
