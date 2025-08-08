c10f3d1369640a589e61e7f37a8d7612d88b4b35
Naing Htet Zaw <nainghtetzaw20101@gmail.com>
2025-08-08 18:10:39 +0700

    finished solving TappingRainWater problem

---
+++ Two Pointer/TappingRainWater.py
@@ -0,0 +1,25 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if len(height) < 3:
+            return 0
+
+        l, r, res = 0, len(height) - 1, 0
+        maxL, maxR = height[l], height[r]
+
+        while l <= r:
+            if maxL < maxR or maxL == maxR:
+                if maxL - height[l] >= 0:
+                    res += maxL - height[l]
+                else:
+                    maxL = height[l]
+                
+                l += 1
+            elif maxR < maxL:
+                if maxR - height[r] >= 0:
+                    res += maxR - height[r]
+                else:
+                    maxR = height[r]
+
+                r -= 1
+
+        return res        
