be203eb6563afb4f8e3c6e3aaabe69776815114d
Naing Htet Zaw <nainghtetzaw20101@gmail.com>
2025-08-12 22:40:07 +0700

    fixed: checkInclusion problem with sorting (Sliding window) (Need to improve)

--- SlidingWindow/CheckInclusion.py
+++ SlidingWindow/CheckInclusion.py
@@ -1,14 +1,21 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        minS = min(s1, s2)
+        if len(s1) > len(s2):
+            return False
 
-        for l in range(len(max(s1, s2))):
+        if len(s1) == len(s2):
+            return sorted(s1) == sorted(s2)
+        
+        minS = min(s1, s2, key = len)
+        maxS = max(s1, s2, key = len)
+
+        for l in range(len(maxS)):
             r = l + len(minS)
 
-            if r >= len(max(s1, s2)):
+            if r > len(maxS):
                 break
             
-            if sorted(minS) == sorted(max(s1, s2)[l:r]):
+            if sorted(minS) == sorted(maxS[l:r]):
                 return True
 
         return False
