be95cb132f8324fe8c7f3be8386a77071440b1d8
Naing Htet Zaw <nainghtetzaw20101@gmail.com>
2025-09-23 07:48:50 +0700

    solved: LargestRectangleArea problem again (Looking at the solution)

--- Stacks/LargestRectangleArea.py
+++ Stacks/LargestRectangleArea.py
@@ -1,24 +1,21 @@
+from typing import List
+
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        res = 0
-        for i in range(len(heights)):
-            prevRect, nextRect = i, i + 1
-            area = 0
-            
-            while prevRect >= 0:
-                if heights[prevRect] < heights[i]:
-                    break
-                area += heights[i]
-                prevRect -= 1
-                
-            
-            while nextRect < len(heights):
-                if heights[nextRect] < heights[i]:
-                    break
-                area += heights[i]
-                nextRect += 1
-            
-            res = max(res, area)
+        maxArea = 0
+        stack = [] # Pair(i, h)
+
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                index, height = stack[-1]
+                maxArea = max(maxArea, height * (i - index))
+                start = index
+                stack.pop()
+            stack.append((start, h))
         
-        return res
+        for i, h in stack:
+            maxArea = max(maxArea, h * (len(heights) - i))
+
+        return maxArea
                      
