a403fb2a23aa133bd11baea20f52cda625bbf87a
Naing Htet Zaw <nainghtetzaw20101@gmail.com>
2025-09-25 22:12:49 +0700

    solved: CarFleet again (had to watch the solution yikes)

--- Stacks/CarFleet.py
+++ Stacks/CarFleet.py
@@ -1,11 +1,12 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pair = [[p, s] for p, s in zip(position, speed)]
+        cars = [(p, s) for p, s in zip(position, speed)]
+        cars.sort(reverse=True)
         stack = []
 
-        for p, s in sorted(pair)[::-1]:
+        for p, s in cars:
             stack.append((target - p) / s)
-            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+            if len(stack) >= 2 and stack[-2] >= stack[-1]:
                 stack.pop()
-
+        
         return len(stack)
