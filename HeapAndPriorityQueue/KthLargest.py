8000e76054c4103b13a1741e64c3be340deda70a
Naing Htet Zaw <nainghtetzaw20101@gmail.com>
2025-10-22 06:50:06 +0700

    solved: Kth Largest Element in a stream problem

--- HeapAndPriorityQueue/KthLargest.py
+++ HeapAndPriorityQueue/KthLargest.py
@@ -1,3 +1,4 @@
+import heapq
 from typing import List
 
 class KthLargest:
@@ -5,8 +6,18 @@ class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         self.k = k
         self.nums = nums
+        heapq.heapify(self.nums)
+
+        while len(self.nums) > k:
+            heapq.heappop(self.nums)
 
     def add(self, val: int) -> int:
-        self.nums.append(val)
-        self.nums.sort()
-        return self.nums[len(self.nums) - self.k]
+        heapq.heappush(self.nums, val)
+        
+        if len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+        return self.nums[0]
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
