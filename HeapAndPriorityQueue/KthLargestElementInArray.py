70a310bf04791b7d98ea1a832bde737190e0361e
Naing Htet Zaw <nainghtetzaw20101@gmail.com>
2025-10-23 06:53:09 +0700

    solved: Kth Largest Element in Array problem (Need to optimize)

---
+++ HeapAndPriorityQueue/KthLargestElementInArray.py
@@ -0,0 +1,8 @@
+import heapq
+from typing import List
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        res = heapq.nlargest(k, nums)
+        return res[-1]
