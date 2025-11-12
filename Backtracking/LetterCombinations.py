be9eccd5eb4dcee6d4029454b8fb4a7be0768017
Naing Htet Zaw <nainghtetzaw20101@gmail.com>
2025-11-12 07:28:21 +0630

    solved: LetterCombinations problem

---
+++ Backtracking/LetterCombinations.py
@@ -0,0 +1,29 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        hm = {
+            '2': ["a", "b", "c"],
+            '3': ["d", "e", "f"], 
+            '4': ["g", "h", "i"], 
+            '5': ["j", "k", "l"], 
+            '6': ["m", "n", "o"], 
+            '7': ["p", "q", "r", "s"],
+            '8': ["t", "u", "v"],
+            '9': ["w", "x", "y", "z"]
+        }
+        res = []
+
+        def dfs(cur, s):
+            if cur >= len(digits):
+                res.append("".join(s))
+                return
+            
+            for i in hm[digits[cur]]:
+                s.append(i)
+                dfs(cur + 1, s)
+                s.pop()
+        
+        dfs(0, [])
+        return res
