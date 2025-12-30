fb4d6c15f5dcc6e6c6e6ddd8e5491d0e3d36b79d
Naing Htet Zaw <nainghtetzaw20101@gmail.com>
2025-12-30 14:59:27 +0630

    optimized PalindromeSubstring solution

--- 1D_DP/PalindromeSubstring.py
+++ 1D_DP/PalindromeSubstring.py
@@ -1,18 +1,19 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        res = []
+        count = 0
+        stringLen = len(s)
 
-        for i in range(len(s)):
+        for i in range(stringLen):
             l, r = i, i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                res.append(s[l:r + 1])
+            while l >= 0 and r < stringLen and s[l] == s[r]:
+                count += 1
                 l -= 1
                 r += 1
             
             l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                res.append(s[l:r + 1])
+            while l >= 0 and r < stringLen and s[l] == s[r]:
+                count += 1
                 l -= 1
                 r += 1
-                
-        return len(res)
+
+        return count
