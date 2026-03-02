94b8b69aa2c0ac347fc91806d6ddecb6df459035
Naing Htet Zaw <nainghtetzaw20101@gmail.com>
2026-03-02 12:07:04 +0700

    solved Word Search problem

--- Backtracking/WordSearch.py
+++ Backtracking/WordSearch.py
@@ -2,26 +2,30 @@ from typing import List
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        ROWS, COLS = len(board), len(board[0])
-        path = set()
+        ROW, COLUMN, WORDLEN = len(board), len(board[0]), len(word)
 
-        def dfs(row, col, i) -> bool:
-            if i == len(word):
+        def bfs(r, c, index, stack):
+            if index > WORDLEN - 1:
                 return True
-            
-            if (row < 0 or col < 0 or row >= ROWS or col >= COLS or (row, col) in path or word[i] != board[row][col]):
+
+            if (r > ROW - 1 or r < 0
+                or c > COLUMN - 1 or c < 0
+                or board[r][c] != word[index]
+                or (r, c) in stack):
                 return False
-            
-            path.add((row, col))
-            res = (dfs(row + 1, col, i + 1) or
-                    dfs(row - 1, col, i + 1) or
-                    dfs(row, col + 1, i + 1) or
-                    dfs(row, col - 1, i + 1))
-            path.remove((row, col))
-            return res
+
+            stack.append((r, c))
+            result = (bfs(r + 1, c, index + 1, stack) 
+                or bfs(r - 1, c, index + 1, stack)
+                or bfs(r, c + 1, index + 1, stack)
+                or bfs(r, c - 1, index + 1, stack))
+            stack.pop()
+
+            return result
+
+        for r in range(ROW):
+            for c in range(COLUMN):
+                if bfs(r, c, 0, []):
+                    return True
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if dfs(r, c, 0): return True
-            
         return False
